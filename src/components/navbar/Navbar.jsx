f7c5524ccd041c45314d9b1fdbba26a8c9e1716b
NRS\nrsha <nrshakib007@gmail.com>
2024-05-11 17:10:47 +0600

    Added Necessary Components

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>Logo</div>
+      <div>
+        <Link href="/">Homepage</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <Link href="/blog">Blog</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
