c8f8f62958181988127a761bf6fdb6e8fcd424c1
NRS\nrsha <nrshakib007@gmail.com>
2024-05-12 14:34:33 +0600

    Added Footer Design

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,13 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex justify-between items-center px-8 mb-4">
+      <div className="text-2xl font-bold text-gray-400">Blog Site</div>
+      <div className="text-sm text-gray-500 font-semibold">
+        <p>Creative Blogging Agency</p>
+        <p>All Rights Reserved</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
