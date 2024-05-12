7a9a42bd2f80fc689857b2038ceb701cf3394ea9
NRS\nrsha <nrshakib007@gmail.com>
2024-05-12 16:54:52 +0600

    Added About Page Design

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,13 +2,36 @@ import Image from "next/image";
 
 const About = () => {
   return (
-    <div>
-      <Image
-        src="https://images.pexels.com/photos/261579/pexels-photo-261579.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt="about"
-        width={500}
-        height={500}
-      />
+    <div className="flex gap-24">
+      <div className="flex-1 flex flex-col gap-12">
+        <h2 className="text-xl font-bold text-btn">About Agency</h2>
+        <h1 className="text-4xl font-bold">
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className="text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus sed
+          nobis repellat ullam, officia atque, quae mollitia eaque, repellendus
+          commodi suscipit dolorum doloribus eveniet deleniti. Eveniet est
+          placeat facere natus.
+        </p>
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="text-3xl font-bold text-btn">10 K+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div>
+            <h1 className="text-3xl font-bold text-btn">234 K+</h1>
+            <p>People Reached</p>
+          </div>
+          <div>
+            <h1 className="text-3xl font-bold text-btn">5 K+</h1>
+            <p>Services and Plugins</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex-1">
+        <Image src="/about.png" alt="" width={500} height={200} />
+      </div>
     </div>
   );
 };
